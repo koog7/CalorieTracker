447b6c5db97497521d5d9c466bf6b1adb943f158
koog7 <k.ognev709@mail.com>
2024-07-07 15:50:16 +0600

    Добавлены функции отслеживания value в input и select , и добавлена функция для Post запроса

--- src/containers/Edit.tsx
+++ src/containers/Edit.tsx
@@ -1,5 +1,42 @@
-import { TextField, Button, MenuItem, Select, FormControl, InputLabel, Box } from '@mui/material';
+import { TextField, Button, MenuItem, Select, FormControl, InputLabel, Box, SelectChangeEvent } from '@mui/material';
+import {ChangeEvent, useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import axiosAPI from "../axios/AxiosAPI.tsx";
+
+interface Meal {
+    reception: string;
+    description: string;
+    kcal: number | null;
+}
+
 const Edit = () => {
+
+    const [mealInfo, setMealInfo] = useState<Meal>({
+        reception: 'Breakfast',
+        description: '',
+        kcal: null,
+    });
+    const navigate = useNavigate();
+    const getReception = (event: SelectChangeEvent) => {
+        setMealInfo({ ...mealInfo, reception: event.target.value});
+    };
+    const getDescription = (event: ChangeEvent<HTMLInputElement>) => {
+        setMealInfo({ ...mealInfo, description: event.target.value });
+    };
+    const getKcal = (event: ChangeEvent<HTMLInputElement>) => {
+        setMealInfo({ ...mealInfo, kcal: event.target.value as number});
+    };
+
+    useEffect(() => {
+        console.log('Meal Info:', mealInfo);
+    }, [mealInfo]);
+
+    const postData = async () => {
+        if(mealInfo.description.trim() !== '' && mealInfo.kcal !== null){
+            await axiosAPI.post('/meal.json',mealInfo);
+            await navigate('/')
+        }
+    }
     return (
         <div>
             <h1 style={{textAlign:'center'}}>Add / Edit meal</h1>
@@ -7,6 +44,8 @@ const Edit = () => {
                 <FormControl fullWidth sx={{ color: 'white' }}>
                     <InputLabel sx={{ color: 'white' }}>Reception time</InputLabel>
                     <Select
+                        value={mealInfo.reception}
+                        onChange={getReception}
                         label="Reception time"
                         sx={{
                             '.MuiOutlinedInput-notchedOutline': { borderColor: 'white' },
@@ -24,6 +63,8 @@ const Edit = () => {
                 </FormControl>
                 <TextField
                     fullWidth
+                    value={mealInfo.description}
+                    onChange={getDescription}
                     label="Meals"
                     variant="outlined"
                     InputLabelProps={{ style: { color: 'white' } }}
@@ -36,8 +77,11 @@ const Edit = () => {
                 />
                 <TextField
                     fullWidth
+                    value={mealInfo.kcal}
+                    onChange={getKcal}
                     label="kcal"
                     variant="outlined"
+                    type="number"
                     InputLabelProps={{ style: { color: 'white' } }}
                     sx={{
                         input: { color: 'white' },
@@ -48,6 +92,7 @@ const Edit = () => {
                 />
                 <Button
                     variant="contained"
+                    onClick={postData}
                     sx={{
                         backgroundColor: 'white',
                         color: 'black',
