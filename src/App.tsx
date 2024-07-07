69affbe7bfd4fd1feeaa4fd210b68d47f5936007
koog7 <k.ognev709@mail.com>
2024-07-07 12:18:28 +0600

    Сделан примерный дизайн страницы для подсчёта каллорий

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import Home from "./containers/Home.tsx";
+import {Route, Routes} from "react-router-dom";
+
 
 const App = () => {
 
     return(
         <>
-
+            <div style={{backgroundColor: '#404040', width: '1000px', minHeight: '50px', padding: '2px'}}>
+                <h3 style={{marginLeft: '10px'}}>Calorie Tracker</h3>
+            </div>
+            <hr/>
+            <Routes>
+                <Route path="/" element={(
+                    <Home/>
+                )}/>
+            </Routes>
         </>
     )
 
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,7 +1,34 @@
+import {Button} from "@mui/material";
+import {NavLink} from "react-router-dom";
+
 const Home = () => {
     return (
         <div>
-
+            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%'}}>
+                <div>
+                    <h3>Total Calories: {123}</h3>
+                </div>
+                <div>
+                    <NavLink className="nav-link" to="/"><Button variant="outlined">Add new meal</Button></NavLink>
+                </div>
+            </div>
+            <div style={{border: '1px solid white', padding:'10px',borderRadius:'5px'}}>
+                <div className={'card'} style={{display:'flex', alignItems: 'center'}}>
+                    <div>
+                        <p>Reception time</p>
+                        <p>Type meal</p>
+                    </div>
+                    <div style={{marginLeft:'auto'}}>
+                        <p style={{marginRight:'50px', fontSize:'20px'}}>
+                            {600} call
+                        </p>
+                    </div>
+                    <div style={{display:'flex',flexDirection:'column', gap:'10px'}}>
+                        <Button variant="contained" sx={{backgroundColor:'green'}}>Edit</Button>
+                        <Button variant="contained" sx={{backgroundColor:'red'}}>Delete</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
